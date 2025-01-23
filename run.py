c2a2b06ebc9257ef3bf3904ef34bb02327f288ce
Marry699 <2982815993@qq.com>
2025-01-23 17:07:24 +0800

    update

--- run.py
+++ run.py
@@ -40,7 +40,7 @@ if __name__ == "__main__":
     parser.add_argument('--nodedim', type=int, default=10)
 
     # optimization
-    parser.add_argument("--num_workers", type=int, default=0, help="data loader num workers")
+    parser.add_argument("--num_workers", type=int, default=10, help="data loader num workers")
     parser.add_argument("--itr", type=int, default=1, help="experiments times")
     parser.add_argument("--train_epochs", type=int, default=10, help="train epochs")
     parser.add_argument("--batch_size", type=int, default=64, help="batch size of train input data")
